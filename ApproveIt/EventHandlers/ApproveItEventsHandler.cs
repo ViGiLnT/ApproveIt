65b63a3d87da96f063e2733e00044be0e2330ce0
andresantos <andre.santos@create.pt>
2016-01-04 19:11:37 +0000

    Started new feature where the content changes are tracked.

---
+++ ApproveIt/EventHandlers/ApproveItEventsHandler.cs
@@ -0,0 +1,84 @@
+﻿namespace Create.Plugin.ApproveIt.EventHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using Newtonsoft.Json;
+    using Pocos;
+    using Umbraco.Core;
+    using Umbraco.Core.Events;
+    using Umbraco.Core.Models;
+    using Umbraco.Core.Models.Membership;
+    using Umbraco.Core.Persistence;
+    using Umbraco.Core.Publishing;
+    using Umbraco.Core.Services;
+    using Umbraco.Web;
+
+    /// <summary>
+    /// Application Events Handler.
+    /// </summary>
+    public class ApproveItEventsHandler : ApplicationEventHandler
+    {
+        /// <summary>
+        /// Event that runs when the application is first started.
+        /// </summary>
+        /// <param name="umbracoApplication">The umbraco application.</param>
+        /// <param name="applicationContext">The application context.</param>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            //////Get the Umbraco Database context
+            ////var db = applicationContext.DatabaseContext.Database;
+
+            ////DatabaseSchemaHelper helper = new DatabaseSchemaHelper(db,);
+
+            //////Check if the DB table does NOT exist
+            ////if (!db.TableExist("ChangeHistory"))
+            ////{
+            ////    //Create DB table - and set overwrite to false
+            ////    db.CreateTable<ChangeHistory>(false);
+            ////}
+
+            ContentService.SendingToPublish += SendingToPublishEventHandler;
+        }
+
+        /// <summary>
+        /// Sendings to publish event handler.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="args">The <see cref="SendToPublishEventArgs{IContent}"/> instance containing the event data.</param>
+        private void SendingToPublishEventHandler(IContentService sender, SendToPublishEventArgs<IContent> args)
+        {
+            if (args == null || args.Entity == null || args.Entity.Properties == null || args.Entity.Properties.Count == 0)
+            {
+                return;
+            }
+
+            // Get user making changes
+            IUser user = UmbracoContext.Current.Security.CurrentUser;
+
+            // Fetch original content
+            IContent originalContent = UmbracoContext.Current.Application.Services.ContentService.GetById(args.Entity.Id);
+
+            // Find which properties were actually changed
+            List<Property> changedProperties = new List<Property>();
+
+            IList<Property> dirtyProps = args.Entity.Properties.ToList();
+            foreach (Property dirtyProp in dirtyProps)
+            {
+                Property originalProp = originalContent.Properties
+                    .Where(x => x.Id == dirtyProp.Id)
+                    .FirstOrDefault();
+
+                if (originalProp != null && 
+                    originalProp.Value != null && 
+                    string.Compare(originalProp.Value.ToString(), dirtyProp.Value.ToString(), false) != 0)
+                {
+                    changedProperties.Add(dirtyProp);
+                }
+            }
+
+            // Store the changed properties in the ChangeHistory table
+        }
+    }
+}
---
+++ ApproveIt/Pocos/ChangeHistory.cs
@@ -0,0 +1,84 @@
+﻿namespace Create.Plugin.ApproveIt.Pocos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using Umbraco.Core.Persistence;
+    using Umbraco.Core.Persistence.DatabaseAnnotations;    
+    
+    /// <summary>
+    /// PetaPoco class that represents the ChangeHistory DB Table
+    /// </summary>
+    [TableName("ChangeHistory")]
+    [PrimaryKey("Id", autoIncrement = true)]
+    [ExplicitColumns]
+    public class ChangeHistory
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        [Column("id")]
+        [PrimaryKeyColumn(AutoIncrement = true)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the node identifier.
+        /// </summary>
+        /// <value>
+        /// The node identifier.
+        /// </value>
+        [Column("nodeId")]
+        public int NodeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property identifier.
+        /// </summary>
+        /// <value>
+        /// The property identifier.
+        /// </value>
+        [Column("propertyId")]
+        public int PropertyId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the update date.
+        /// </summary>
+        /// <value>
+        /// The update date.
+        /// </value>
+        [Column("updated")]
+        public DateTime UpdateDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the updated by col.
+        /// </summary>
+        /// <value>
+        /// The updated by col.
+        /// </value>
+        [Column("updatedBy")]
+        public string UpdatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the previous value.
+        /// </summary>
+        /// <value>
+        /// The previous value.
+        /// </value>
+        [Column("previousValue")]
+        [SpecialDbType(SpecialDbTypes.NTEXT)]
+        public string PreviousValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current value.
+        /// </summary>
+        /// <value>
+        /// The current value.
+        /// </value>
+        [Column("currentValue")]
+        [SpecialDbType(SpecialDbTypes.NTEXT)]
+        public string CurrentValue { get; set; }
+    }
+}
